PixelExperience/frameworks_opt_telephony

PhoneFactory: make old qti telephony optional
* breaks pie qti telephony

Change-Id: I3c01d310ae65dac2fb20388bf26905acd694161d

src/java/com/android/internal/telephony/PhoneFactory.java
@@ -24,6 +24,7 @@ import android.content.pm.PackageManager;
 import android.net.LocalServerSocket;
 import android.os.Looper;
 import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
@@ -227,8 +228,11 @@ public class PhoneFactory {
                 sMadeDefaults = true;
 
                 Rlog.i(LOG_TAG, "Creating SubInfoRecordUpdater ");
-                sSubInfoRecordUpdater = telephonyComponentFactory.makeSubscriptionInfoUpdater(
-                        context, sPhones, sCommandsInterfaces);
+                sSubInfoRecordUpdater = SystemProperties.get("ro.vendor.qti.pie.telephony", "0").equals("0")
+                        ? telephonyComponentFactory.makeSubscriptionInfoUpdater(
+                                context, sPhones, sCommandsInterfaces)
+                        : telephonyComponentFactory.makeSubscriptionInfoUpdater(
+                                BackgroundThread.get().getLooper(), context, sPhones, sCommandsInterfaces);
                 SubscriptionController.getInstance().updatePhonesAvailability(sPhones);
 
                 // Start monitoring after defaults have been made.

kotori17/android_frameworks_opt_telephony

src/java/com/android/internal/telephony/PhoneFactory.java
@@ -24,6 +24,7 @@ import android.content.pm.PackageManager;
 import android.net.LocalServerSocket;
 import android.os.Looper;
 import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
@@ -227,8 +228,11 @@ public class PhoneFactory {
                 sMadeDefaults = true;
 
                 Rlog.i(LOG_TAG, "Creating SubInfoRecordUpdater ");
-                sSubInfoRecordUpdater = telephonyComponentFactory.makeSubscriptionInfoUpdater(
-                        context, sPhones, sCommandsInterfaces);
+                sSubInfoRecordUpdater = SystemProperties.get("persist.vendor.qti.pie.telephony", "0").equals("0")
+                        ? telephonyComponentFactory.makeSubscriptionInfoUpdater(
+                                context, sPhones, sCommandsInterfaces)
+                        : telephonyComponentFactory.makeSubscriptionInfoUpdater(
+                                BackgroundThread.get().getLooper(), context, sPhones, sCommandsInterfaces);
                 SubscriptionController.getInstance().updatePhonesAvailability(sPhones);
 
                 // Start monitoring after defaults have been made.